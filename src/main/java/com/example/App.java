3428ee85ae142c9f7d70bc40cdeffe5fe1985cbd
yogi7302 <mpyogi2226@gmail.com>
2025-10-22 14:47:10 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,10 @@
-package com.example;
-
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        port(9090); // Use 9090 to avoid Jenkins port conflict
-        get("/", (req, res) -> "Hello, Maven Build via Jenkins AND BY YOGESH!");
-
-        // SparkJava keeps the JVM alive automatically
-        System.out.println("Server started at http://localhost:9090");
+        port(8080); // internal container port
+        ipAddress("0.0.0.0"); // bind to all network interfaces
+        get("/", (req, res) -> "Hello, Maven Build via Jenkins AND BY YOGESH!....");
+        System.out.println("Server started at http://0.0.0.0:8080");
     }
 }
