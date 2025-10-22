a9fbb8d1479d44835ae738f521f12b87faac464b
yogi7302 <mpyogi2226@gmail.com>
2025-10-22 14:08:04 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, Maven Build via Jenkins AND BY YOGESH!....");
+        // Keep the app running indefinitely
+        while (true) {
+            Thread.sleep(10000); // sleep 10 seconds
+        }
     }
 }
