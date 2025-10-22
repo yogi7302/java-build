75415131be0a5b0e2b8bb69e3aca62ba48bd9b1e
yogi7302 <mpyogi2226@gmail.com>
2025-10-22 16:38:29 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,11 @@
+package com.example;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        port(8080); // internal container port
-        ipAddress("0.0.0.0"); // bind to all network interfaces
+        port(8080);
+        ipAddress("0.0.0.0");
         get("/", (req, res) -> "Hello, Maven Build via Jenkins AND BY YOGESH!....");
         System.out.println("Server started at http://0.0.0.0:8080");
     }
