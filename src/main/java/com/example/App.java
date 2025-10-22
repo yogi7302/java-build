1b85f1efe1da365ed6a1bab8455c210d5ea13149
yogi7302 <mpyogi2226@gmail.com>
2025-10-22 22:24:53 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         port(8080);
         ipAddress("0.0.0.0");
-        get("/", (req, res) -> "Hello, Maven Build via Jenkins AND BY YOGESH!....FINALLY I DID IT...10,0000+ AURA...DOES it work..?");
+        get("/", (req, res) -> "Hello, i am yogesh");
         System.out.println("Server started at http://0.0.0.0:8080");
     }
 }
