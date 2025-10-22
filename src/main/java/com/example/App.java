0a8cff7c1c1a34ca960f60bc31bebe1d5bddd730
yogi7302 <mpyogi2226@gmail.com>
2025-10-22 14:21:22 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,13 @@
+package com.example;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        port(8080); // inside container
+        port(8080); // Container listens on 8080
         get("/", (req, res) -> "Hello, Maven Build via Jenkins AND BY YOGESH!....");
+
+        // Keep the server running indefinitely
+        // SparkJava keeps the JVM alive, no need for manual while loop
     }
 }
