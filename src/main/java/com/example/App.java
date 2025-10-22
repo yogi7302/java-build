c8c959d26845e6e1932d29a592f4ea738e7e27db
yogi7302 <mpyogi2226@gmail.com>
2025-10-22 13:33:36 +0530

    Update print statement in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven Build via Jenkins AND BY YOGESH!");
+        System.out.println("Hello, Maven Build via Jenkins AND BY YOGESH!....");
     }
 }
